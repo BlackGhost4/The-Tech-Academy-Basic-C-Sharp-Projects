2f4b53e70489ed43aa4fa854a56cc850c66ba0dd
BlackGhost4 <60488987+BlackGhost4@users.noreply.github.com>
2021-11-30 12:22:38 -0800

    Add a console app assignment (step 185)

---
+++ Basic_C#_Programs/ConsoleAppStep185/ConsoleAppStep185/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleAppStep185
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //the program asks the user for a number
+            Console.WriteLine("Enter a number");
+            string numberString = Console.ReadLine();
+            //The number cast to type "long", to have a value of more than 10,000,000
+            long number = Convert.ToInt64(numberString);
+            //The number is multiplied by 50, and the print the result in the console
+            Console.WriteLine(number + " multiplied by 50 = " + (number * 50));
+            //The number is added 25, and the print the result in the console
+            Console.WriteLine(number + " plus 25 = " + (number + 25));
+            //The number is divided by 50, and the print the result (cast to Double) in the console
+            Console.WriteLine(number + " divided by 50 = " + Convert.ToDouble(number / 12.5));
+            //The number is greater than 50? The result (boolean) cast to string and the print in the console
+            Console.WriteLine(number + " > 50? " + (number > 50).ToString());
+            //Print in the console the remainder of the number divided by 7
+            Console.WriteLine("The remainder of " + number + " divided by 7 = " + (number % 7).ToString());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic_C#_Programs/MathChallenge/MathChallenge/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MathChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberA = 20;
+            int numberB = 10;
+
+            int sum = numberA + numberB;
+            int subtraction = numberA - numberB;
+            int multiplication = numberA * numberB;
+            int division = numberA / numberB;
+
+            Console.WriteLine(numberA.ToString() + " + " + numberB.ToString() + " = " + sum.ToString());
+            Console.WriteLine(numberA.ToString() + " - " + numberB.ToString() + " = " + subtraction.ToString());
+            Console.WriteLine(numberA.ToString() + " * " + numberB.ToString() + " = " + multiplication.ToString());
+            Console.WriteLine(numberA.ToString() + " / " + numberB.ToString() + " = " + division.ToString());
+            Console.ReadLine();
+        }
+    }
+}
