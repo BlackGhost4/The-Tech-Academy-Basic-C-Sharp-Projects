f73fe4404bf73fadc5142c8ec4bce1809995bb06
BlackGhost4 <60488987+BlackGhost4@users.noreply.github.com>
2021-11-30 14:34:24 -0800

    BOOLEAN LOGIC SUBMISSION ASSIGNMENT (step 199)

---
+++ Basic_C#_Programs/BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(false && true && true || true);
+            //Console.WriteLine(true && false);
+            //Console.WriteLine(true && true);
+            //Console.WriteLine(false && false);
+
+            //Console.WriteLine(true || true);
+            //Console.WriteLine(true || false);
+            //Console.WriteLine(false || false);
+
+            //Console.WriteLine(true == true);
+            //Console.WriteLine(true == false);
+            //Console.WriteLine(false == true);
+
+            //Console.WriteLine(true != true);
+            //Console.WriteLine(true != false);
+            //Console.WriteLine(false != true);
+
+            Console.WriteLine(true ^ true ^ false);
+            Console.WriteLine(true ^ false ^ false);
+            Console.WriteLine(false ^ false);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic_C#_Programs/BooleanLogicStep199/BooleanLogicStep199/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace BooleanLogicStep199
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("BOOLEAN LOGIC SUBMISSION ASSIGNMENT");
+            //The program ask the user's age
+            Console.WriteLine("\nWhat is your age?");
+            //Store the vaue in a varible "age" of int type
+            int age = Convert.ToInt32(Console.ReadLine());
+            //The program ask if the user ever had a DUI
+            Console.WriteLine("\nHave you ever had a DUI? (true or false)");
+            //Store the vaue in a varible "haveEverDui" of bool type
+            bool haveEverDui = Convert.ToBoolean(Console.ReadLine());
+            //The program ask the number of speeding tickets the user has
+            Console.WriteLine("\nHow many speeding tickets do you have?");
+            //Store the vaue in a varible "speedingTickets" of int type
+            int speedingTickets = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nApplicant qualifies for car insurance?");
+            /* Print the result in the console with the following rules
+            A. Applicants must be over 15 years old.
+            B. Applicants must not have any DUI’s.
+            C. Applicants must not have more than 3 speeding tickets. */
+            Console.WriteLine(age > 15 && haveEverDui == false && speedingTickets <= 3);
+            Console.ReadLine();
+        }
+    }
+}
