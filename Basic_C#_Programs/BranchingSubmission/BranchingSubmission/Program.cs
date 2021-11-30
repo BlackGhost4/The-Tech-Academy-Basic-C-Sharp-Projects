e913045121fcc2d2548a4f3c1d6aca48f9ec69de
BlackGhost4 <60488987+BlackGhost4@users.noreply.github.com>
2021-11-30 15:29:23 -0800

    BRANCHING SUBMISSION ASSIGNMENT (step 222)

---
+++ Basic_C#_Programs/BranchingSubmission/BranchingSubmission/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace BranchingSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Print the header in the console
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            //Package weight is requested
+            Console.WriteLine("what is the weight of the package?");
+            int packageWeight = Convert.ToInt32(Console.ReadLine());
+            if (packageWeight > 50)
+            {
+                Console.WriteLine("Error: Package too heavy to be shipped via Package Express. Have a good day.");
+            }
+            else
+            {
+                Console.WriteLine("what is the width of the package?");
+                int packageWidth = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("what is the height of the package?");
+                int packageHeight = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("what is the length of the package?");
+                int packageLength = Convert.ToInt32(Console.ReadLine());
+                if ((packageWidth + packageHeight + packageLength) > 50)
+                {
+                    Console.WriteLine("Error: Package too big to be shipped via Package Express.");
+                }
+                else
+                {
+                    decimal totalForShipping = ((packageWidth * packageHeight * packageLength) * packageWeight) / 100;
+                    Console.WriteLine("Your estimated total for shipping this package is: $" + string.Format("{0:0.00}", totalForShipping) + " USD");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic_C#_Programs/ConditionalStatements/ConditionalStatements/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConditionalStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number != 0)
+            {
+                if (number <= 10)
+                {
+                    Console.WriteLine("The number is less or equal than 10");
+                }
+                else if (number <= 20)
+                {
+                    Console.WriteLine("The number is less or equal than 20");
+                }
+                else if (number > 20 && number <= 50)
+                {
+                    Console.WriteLine("The number is more than 20 and less or equal than 50");
+                }
+                else
+                {
+                    Console.WriteLine("The number is more than 50");
+                }
+            }
+            else
+            {
+                Console.WriteLine("The number is zero");
+            }
+            Console.WriteLine(number < 0 ? "The number is negative" : number == 0 ? "The number is zero" : "The number is positive");
+            Console.ReadLine();
+        }
+    }
+}
