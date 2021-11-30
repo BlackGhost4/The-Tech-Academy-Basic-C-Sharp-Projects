29019c252951dd9da2a90a77c946dd366f1da1c8
BlackGhost4 <60488987+BlackGhost4@users.noreply.github.com>
2021-11-30 15:33:25 -0800

    Add the comments

--- Basic_C#_Programs/BranchingSubmission/BranchingSubmission/Program.cs
+++ Basic_C#_Programs/BranchingSubmission/BranchingSubmission/Program.cs
@@ -11,28 +11,37 @@ namespace BranchingSubmission
             //Package weight is requested
             Console.WriteLine("what is the weight of the package?");
             int packageWeight = Convert.ToInt32(Console.ReadLine());
+            //If weight of package is more than 50, print a message in the console and end the program
             if (packageWeight > 50)
             {
                 Console.WriteLine("Error: Package too heavy to be shipped via Package Express. Have a good day.");
-            }
+            } 
+            //else
             else
             {
+                //Package width is requested
                 Console.WriteLine("what is the width of the package?");
                 int packageWidth = Convert.ToInt32(Console.ReadLine());
+                //Package height is requested
                 Console.WriteLine("what is the height of the package?");
                 int packageHeight = Convert.ToInt32(Console.ReadLine());
+                //Package length is requested
                 Console.WriteLine("what is the length of the package?");
                 int packageLength = Convert.ToInt32(Console.ReadLine());
+                //if the total dimensions greather than 50, print the message in the console and end the program
                 if ((packageWidth + packageHeight + packageLength) > 50)
                 {
                     Console.WriteLine("Error: Package too big to be shipped via Package Express.");
                 }
+                //else
                 else
                 {
+                    //Calculate the estimated total for shipping for the package
                     decimal totalForShipping = ((packageWidth * packageHeight * packageLength) * packageWeight) / 100;
                     Console.WriteLine("Your estimated total for shipping this package is: $" + string.Format("{0:0.00}", totalForShipping) + " USD");
                 }
             }
+            //wait until the user presses key enter
             Console.ReadLine();
         }
     }
