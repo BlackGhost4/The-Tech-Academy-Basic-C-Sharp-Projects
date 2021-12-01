46ccb190080d8cbf8e0d8611e368d3f321b4f90e
BlackGhost4 <60488987+BlackGhost4@users.noreply.github.com>
2021-11-30 17:42:01 -0800

    CONSOLE APP ARRAY SUBMISSION ASSIGNMENT (step 242)

---
+++ Basic_C#_Programs/ArraysAndLists/ArraysAndLists/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArraysAndLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> intList = new List<string>();
+
+            intList.Add("Hello");
+            intList.Add("Engels");
+            intList.Remove("Engels");
+            Console.WriteLine(intList[0]);
+
+            //int[] numberArray = new int[5];
+            //numberArray[0] = 5;
+            //numberArray[1] = 2;
+            //numberArray[2] = 10;
+            //numberArray[3] = 200;
+            //numberArray[4] = 5000;
+
+            //int[] numberArray2 = new int[] { 5, 2, 10, 200, 5000, 600, 2300 };
+
+            //int[] numberArray3 = { 5, 2, 10, 200, 5000, 600, 2300 };
+
+            //numberArray3[5] = 650;
+
+            //Console.WriteLine(numberArray3[5]);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic_C#_Programs/ConsoleAppArraySubmission/ConsoleAppArraySubmission/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ConsoleAppArraySubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Arrays are initialized with 8 values each
+            string[] colorArray = { "Yellow", "Red", "Black", "Blue", "White", "Orange", "Purple", "Green" };
+            int[] numberArray = { 5, 4, 9, 67, 1404, 3923, 2343, 10 };
+            //The variable "exitLoop", will store the data entered by the user
+            string exitLoop = "";
+            int index = 0;
+
+            //Print the instructions in the console
+            Console.WriteLine("Welcome to Console App Array Submission");
+            Console.WriteLine("There are two arrangements with 8 elements each. The first array is of \"colors\" and the second is of \"numbers\"");
+            do
+            {
+                //The data is requested, for finish the program the user must type "exit"
+                Console.WriteLine("Enter the value of the index to display an element (0-7). NOTE: For the finish the program enter the word \"exit\"");
+                exitLoop = Console.ReadLine();
+                //In the next line, an attempt is made to pass the entered value to a numeric type
+                if (int.TryParse(exitLoop, out index))
+                {
+                    //In case the data is a number, it is verified that area a number between in 0 and 7
+                    if (index >= 0 && index <= 7)
+                    {
+                        //If it is true, it is printed the value in the position of the arrays
+                        Console.WriteLine("The value of the color in the position " + index + ", is: " + colorArray[index]);
+                        Console.WriteLine("The value of the number in the position " + index + ", is: " + numberArray[index]);
+                    }
+                    else
+                    {
+                        //If it is false, it is printed a message
+                        Console.WriteLine("The number doesn’t exist");
+                    }
+                }
+                //If the data entered is not a number, the program print in the console the message
+                else
+                {
+                    Console.WriteLine("The number is not valid");
+                }
+            //The loop ends when the user type a word "exit"
+            }while (!exitLoop.ToUpper().Equals("EXIT"));
+        }
+    }
+}
