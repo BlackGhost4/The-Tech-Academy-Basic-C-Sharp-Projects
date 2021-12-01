f33921ac56ca9d2883a7b3d0b749c010c1b14efb
BlackGhost4 <60488987+BlackGhost4@users.noreply.github.com>
2021-11-30 18:01:18 -0800

    Add files

---
+++ Basic_C#_Programs/Iteration/Iteration/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Iteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] testScores = { 98, 99, 85, 70, 82, 34, 91, 90, 94 };
+
+            //for (int i = 0; i < testScores.Length; i++)
+            //{
+            //    if (testScores[i] > 85)
+            //    {
+            //        Console.WriteLine("Passing test score: " + testScores[i]);
+            //    }
+            //}
+
+            //string[] names = { "Engels", "Erik", "Daniel", "Adam" };
+            //for (int j = 0; j < names.Length; j++)
+            //{
+            //    Console.WriteLine(names[j]);
+            //}
+
+            //List<int> testScores = new List<int>();
+            //testScores.Add(98);
+            //testScores.Add(99);
+            //testScores.Add(81);
+            //testScores.Add(72);
+            //testScores.Add(70);
+
+            //foreach (int score in testScores)
+            //{
+            //    if (score > 85)
+            //    {
+            //        Console.WriteLine("Passing test score: " + score);
+            //    }
+            //}
+
+            //List<string> names = new List<string>() { "Engels", "Erik", "Adam", "Daniel" };
+            //foreach (string name in names)
+            //{
+            //    Console.WriteLine(name);
+            //}
+
+            List<int> testScores = new List<int>() { 98, 99, 85, 70, 82, 34, 91, 90, 94 };
+            List<int> passsingScores = new List<int>();
+            foreach (int score in testScores)
+            {
+                if (score > 85)
+                {
+                    passsingScores.Add(score);
+                }
+            }
+            Console.WriteLine(passsingScores.Count);
+
+            Console.ReadLine();
+        }
+    }
+}
