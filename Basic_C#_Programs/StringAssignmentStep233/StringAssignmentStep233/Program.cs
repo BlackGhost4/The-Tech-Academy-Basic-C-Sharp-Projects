a20ef68ebc8f7c90501ff7200a1e57e10a5e2e1a
BlackGhost4 <60488987+BlackGhost4@users.noreply.github.com>
2021-11-30 16:36:41 -0800

    STRING ASSIGNMENT (step 233)

---
+++ Basic_C#_Programs/StringAssignmentStep233/StringAssignmentStep233/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+
+namespace StringAssignmentStep233
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Initialize tree string
+            string string1 = "Hello World !!!! ";
+            string string2 = " My name is";
+            string string3 = " Engels";
+
+            //Concat the last string in the "concatString" variable
+            string concatString = string.Concat(string1, string2, string3);
+            Console.WriteLine(concatString);
+            //To uppercase the "concatString" variable
+            Console.WriteLine(concatString.ToUpper());
+
+            //Create a string builder with the name "sb"
+            StringBuilder sb = new StringBuilder();
+            //Append four lines in the variable
+            sb.Append("Hello, my name is Engels");
+            sb.Append("\nI\'m from Mexico");
+            sb.Append("\n and I Like coding");
+            sb.Append("\n\"Coding is my cardio\"");
+            //Print in the console the "sb" variable
+            Console.WriteLine(sb);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic_C#_Programs/StringFunctions/StringFunctions/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace StringFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Engels";
+            //string quote = "The man said: \"Hello\", Engels.\nNew Line.\n\t is a tab";
+            //string fileName = @"C:\Users\Engels";
+
+            //bool trueOrFalse = name.Contains("s");
+            //trueOrFalse = name.EndsWith("s");
+            //int length = name.Length;
+
+            //Console.WriteLine(quote);
+            //Console.WriteLine(fileName);
+            //Console.WriteLine(trueOrFalse);
+            //Console.WriteLine(length);
+            //Console.WriteLine(name.ToUpper());
+            //Console.WriteLine(name.ToLower());
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("My name is Engels");
+
+            Console.ReadLine();
+        }
+    }
+}
