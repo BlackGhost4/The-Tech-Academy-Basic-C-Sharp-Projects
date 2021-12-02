76516cde115ae4b76889e197d8d39b8248d15318
BlackGhost4 <60488987+BlackGhost4@users.noreply.github.com>
2021-12-02 11:34:11 -0800

    MAIN METHOD SUBMISSION ASSIGNMENT (Step 279)

---
+++ Basic_C#_Programs/MainMethodStep279/MainMethodStep279/MathOperation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodStep279
+{
+    //Class x is created
+    class MathOperation
+    {
+        public MathOperation()
+        {
+            //The variable is initialized and it will help us in the mathematical operation of addition
+            auxNumber = 8;
+        }
+        //I use the variable that it will help us in the mathematical operation of addition
+        public int auxNumber { get; set; }
+
+        //The Addition method is created with an integer parameter
+        public int Addition(int wholeNumber)
+        {
+            return wholeNumber + auxNumber;
+        }
+
+        //The Addition method is created with a decimal parameter
+        public int Addition(decimal decimalNumber)
+        {
+            return Convert.ToInt32(decimalNumber) + auxNumber;
+        }
+
+        //The Addition method is created with a string parameter
+        public int Addition(string stringNumber)
+        {
+            //If there is an error, it is transmitted to Main(), where that error is caught and the message is displayed on the console
+            return Convert.ToInt32(stringNumber) + auxNumber;
+        }
+    }
+}
---
+++ Basic_C#_Programs/MainMethodStep279/MainMethodStep279/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MainMethodStep279
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //Class MathOperation is instantiated
+                MathOperation mathOperation = new MathOperation();
+                //For the purposes of the exercise, three variables were created that will serve as parameters in the three methods
+                int numberA = 45;
+                decimal numberB = 235.23455m;
+                string numberC = "23";
+                //The first method is called with an integer as a parameter
+                Console.WriteLine(mathOperation.auxNumber + " + " + numberA + " = " + mathOperation.Addition(numberA));
+                //The second method is called with a decimal as a parameter
+                Console.WriteLine(mathOperation.auxNumber + " + " + numberB + " = " + mathOperation.Addition(numberB));
+                //The third method is called with a string as a parameter
+                Console.WriteLine(mathOperation.auxNumber + " + " + numberC + " = " + mathOperation.Addition(numberC));
+            }
+            //If there is an error during the execution of the program, it is caught and the message is displayed on the console
+            catch (Exception e)
+            {
+                Console.WriteLine("Error!!!! " + e.Message);
+            }
+            finally
+            {
+                //Wait until the user presses the enter key
+                Console.ReadLine();
+            }
+        }
+    }
+}
