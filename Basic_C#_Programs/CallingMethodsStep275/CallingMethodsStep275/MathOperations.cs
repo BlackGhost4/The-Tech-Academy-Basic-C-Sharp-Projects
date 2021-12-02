d34baa9164ee1bbd9da1320355244dd3d98e065a
BlackGhost4 <60488987+BlackGhost4@users.noreply.github.com>
2021-12-02 10:20:42 -0800

    CALLING METHODS SUBMISSION ASSIGNMENT (Step 275)

---
+++ Basic_C#_Programs/CallingMethodsStep275/CallingMethodsStep275/MathOperations.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsStep275
+{
+    //Class "MathOperations" is created
+    class MathOperations
+    {
+        //For the purposes of the exercise, in the class constructor,
+        //an integer variable is initialized that will help us in the mathematical operations
+        public MathOperations()
+        {
+            numberB = 4;
+        }
+        //The variable "numberB" that will help us in the mathematical operations
+        public int numberB { get; set; }
+
+        //Method to add the integer parameter with the variable auxNumber
+        public int Addition(int auxNumber)
+        {
+            //return the result of the mathematical operation
+            return (numberB + auxNumber);
+        }
+
+        //Method to multiplicate the integer parameter with the variable auxNumber
+        public int Multiplication(int auxNumber)
+        {
+            //return the result of the mathematical operation
+            return (numberB * auxNumber);
+        }
+
+        //Method to subtract the integer parameter with the variable auxNumber
+        public int Subtraction(int auxNumber)
+        {
+            //return the result of the mathematical operation
+            return (numberB - auxNumber);
+        }
+    }
+}
---
+++ Basic_C#_Programs/CallingMethodsStep275/CallingMethodsStep275/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CallingMethodsStep275
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //The program request to user a number
+                Console.WriteLine("Type a number to perform three mathematical operations");
+                //the data is stored in the variable number
+                int number = int.Parse(Console.ReadLine());
+                //Class "MathOperations" is instantiated
+                MathOperations mathOperations = new MathOperations();
+                //The method "Addition" is called to add the data entered with the user with the variable that helps us in the operation
+                Console.WriteLine("The number " + mathOperations.numberB + " + " + number + " is equal to " + mathOperations.Addition(number));
+                //The method "Subtraction" is called to subtract the data entered with the user with the variable that helps us in the operation
+                Console.WriteLine("The number " + mathOperations.numberB + " - " + number + " is equal to " + mathOperations.Subtraction(number));
+                //The method "Multiplication" is called to multiplicate the data entered with the user with the variable that helps us in the operation
+                Console.WriteLine("The number " + mathOperations.numberB + " * " + number + " is equal to " + mathOperations.Multiplication(number));
+            }
+            //If there is any exception, catch that exception
+            catch (Exception e)
+            {
+                //print a message error with the exception on the console
+                Console.WriteLine("Error !!!! -> " + e.Message);
+            }
+            finally
+            {
+                //wait until user press enter key
+                Console.ReadLine();
+            }
+        }
+    }
+}
