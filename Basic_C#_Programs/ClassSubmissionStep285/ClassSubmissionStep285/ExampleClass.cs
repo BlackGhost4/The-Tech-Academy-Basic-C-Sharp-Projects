80edfbc6ca445935efec5062195450e51483b59e
BlackGhost4 <60488987+BlackGhost4@users.noreply.github.com>
2021-12-02 15:34:42 -0800

    CLASS SUBMISSION ASSIGNMENT (Step 285)

---
+++ Basic_C#_Programs/ClassSubmissionStep285/ClassSubmissionStep285/ExampleClass.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmissionStep285
+{
+    //******************************************************************************
+    //The static class "ExampleClass" is created
+    //******************************************************************************
+    public static class ExampleClass
+    {
+        /************************************************************************
+         * As the class is static, the method must be static as too
+         * Acept a whole number and is stored in the variable numberA
+         * **********************************************************************/
+        public static void Divide(int numberA, out int numberB)
+        {
+            //If the number can be divided by 2, the mathematical operation is done
+            if ((numberA % 2) == 0)
+            {
+                numberB = numberA / 2;
+            }
+            //otherwise, the variable numberB is assigned a value of -1
+            else
+            {
+                numberB = -1;
+            }
+        }
+
+        /************************************************************************
+         * As the class is static, the method must be static as well
+         * **********************************************************************/
+        public static void Divide(string numberA, out int numberB)
+        {
+            //The variable numberA (string) cast to Integer and the Divide method is called with that value
+            Divide(Convert.ToInt32(numberA), out numberB);
+        }
+    }
+}
---
+++ Basic_C#_Programs/ClassSubmissionStep285/ClassSubmissionStep285/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ClassSubmissionStep285
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                /***************************************************************************************
+                 * The following line was commented, because the class was made static and the static classes are not instantiated
+                
+                ExampleClass exampleClass = new ExampleClass();
+                *****************************************************************************************/
+
+                Console.WriteLine("Press type a whole number");
+                /****************************************************************************************
+                * The Next Line was comented, because the conversion from string to integer is done in a method of the class
+                
+                int number = Convert.ToInt32(Console.ReadLine());
+                ******************************************************************************************/
+                string number = Console.ReadLine();
+                int numberB = 0;
+
+                /***************************************************************************************
+                 * The following line was commented, because the class was made static 
+                
+                exampleClass.Divide(number, out numberB);
+                ****************************************************************************************/
+                ExampleClass.Divide(number, out numberB); //Call the method "Divide" of the static class "ExampleClass"
+                //If the value of numberB is -1, then the data entered by the user cannot divided by two
+                if (numberB == -1)
+                {
+                    //show the message that the number cannot divided by two
+                    Console.WriteLine("The number " + number + " cannot be divided by 2");
+                }
+                else
+                {
+                    //Otherwise, print the result on the console
+                    Console.WriteLine("The number " + number + " divided by 2 is equal to " + numberB);
+                }
+            }
+            //If exist an exception, is catched and print the error message on the console
+            catch (Exception e)
+            {
+                Console.WriteLine("Error!!!! -> " + e.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
