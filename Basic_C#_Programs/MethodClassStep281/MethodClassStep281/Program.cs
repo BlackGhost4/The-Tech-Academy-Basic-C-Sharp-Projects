c0d4f248ef86dbc26c703d52eafe09fbd759a30b
BlackGhost4 <60488987+BlackGhost4@users.noreply.github.com>
2021-12-02 12:51:57 -0800

    Update specification the parameters by name

--- Basic_C#_Programs/MethodClassStep281/MethodClassStep281/Program.cs
+++ Basic_C#_Programs/MethodClassStep281/MethodClassStep281/Program.cs
@@ -11,7 +11,7 @@ namespace MethodClassStep281
             //For purpose of this exercise, the variables numberA and numberB are created
             int numberA = 10, numberB = 0;
             //Call the method "Multiplication" with the two parameters
-            mathOperation.Multiplication(numberA, out numberB);
+            mathOperation.Multiplication(numberA: numberA, numberB: out numberB);
             //The variable "numberB" will store the result of the mathematical operation
             //Print the value of "numberB" on the console
             Console.WriteLine("The mathematical operation " + numberA + " * 4 = {0}", numberB);
