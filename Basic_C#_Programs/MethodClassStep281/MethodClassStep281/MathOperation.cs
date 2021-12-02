04ba48826f9c13ff60df71d1c9bb897f72cff2d5
BlackGhost4 <60488987+BlackGhost4@users.noreply.github.com>
2021-12-02 12:50:33 -0800

    METHOD CLASS SUBMISSION ASSIGNMENT (Step 281)

---
+++ Basic_C#_Programs/MethodClassStep281/MethodClassStep281/MathOperation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassStep281
+{
+    //The class "MathOperation" is created
+    class MathOperation
+    {
+        //The method "Multiplication" is created
+        //This method has a two parameters, the second parameter will store the result of the mathematical operation numberA * 4
+        public void Multiplication(int numberA, out int numberB)
+        {
+            numberB = numberA * 4;
+        }
+    }
+}
---
+++ Basic_C#_Programs/MethodClassStep281/MethodClassStep281/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MethodClassStep281
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Class "MathOperation" is instantiated
+            MathOperation mathOperation = new MathOperation();
+            //For purpose of this exercise, the variables numberA and numberB are created
+            int numberA = 10, numberB = 0;
+            //Call the method "Multiplication" with the two parameters
+            mathOperation.Multiplication(numberA, out numberB);
+            //The variable "numberB" will store the result of the mathematical operation
+            //Print the value of "numberB" on the console
+            Console.WriteLine("The mathematical operation " + numberA + " * 4 = {0}", numberB);
+            Console.ReadLine();
+        }
+    }
+}
