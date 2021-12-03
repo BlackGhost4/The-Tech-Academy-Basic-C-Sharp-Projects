f107f8163fc536d2b024e919c534fec51edf14e3
BlackGhost4 <60488987+BlackGhost4@users.noreply.github.com>
2021-12-03 15:00:48 -0800

    ABSTRACT CLASS SUBMISSION ASSIGNMENT (Step 302)

---
+++ Basic_C#_Programs/AbstractClassStep302/AbstractClassStep302/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassStep302
+{
+    //The class "Employee" is created, and inherit from the "Person" class
+    class Employee : Person
+    {
+        //The virtual method of the "Person" class is implemented in the "Employee" class
+        public override void SayName()
+        {
+            base.SayName();
+        }
+    }
+}
---
+++ Basic_C#_Programs/AbstractClassStep302/AbstractClassStep302/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AbstractClassStep302
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //The "employee" object isinstantiated with FirstName = Sample and LastName = Student
+            Employee employee = new Employee() { FirstName = "Sample", LastName = "Student" };
+            //Call the "SayName" method
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
