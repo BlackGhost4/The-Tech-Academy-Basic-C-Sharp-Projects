af4c44fd039d7c8a701d75373d3c4a78a20b57ab
BlackGhost4 <60488987+BlackGhost4@users.noreply.github.com>
2021-12-03 15:52:41 -0800

    Exercises update

---
+++ Basic_C#_Programs/PolymorphismStep306/PolymorphismStep306/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismStep306
+{
+    //The class "Employee" is created, and inherit from the "Person" class
+    public class Employee : Person, IQuittable
+    {
+        //The virtual method of the "Person" class is implemented in the "Employee" class
+        public override void SayName()
+        {
+            base.SayName();
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine("Quit()");
+        }
+    }
+}
