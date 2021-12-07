25d5c5e2a4a04fc4fbbafd826d5b5d0d66b949f9
BlackGhost4 <60488987+BlackGhost4@users.noreply.github.com>
2021-12-06 16:20:02 -0800

    PARAMETERS ASSIGNMENT(Step 317)

---
+++ Basic_C#_Programs/ParametersStep317/ParametersStep317/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParametersStep317
+{
+    //The class "Employee" is created with a generic type parameter
+    public class Employee<T>
+    {
+        //Add a property to the Employee class called “things”
+        public List<T> things { get; set; }
+    }
+}
--- Basic_C#_Programs/ParametersStep317/ParametersStep317/Program.cs
+++ Basic_C#_Programs/ParametersStep317/ParametersStep317/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ParametersStep317
 {
@@ -6,7 +7,48 @@ namespace ParametersStep317
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Instantiate an Employee object with type “string” as its generic parameter
+            Employee<string> stringNumbers = new Employee<string>();
+            //Assign a list of strings as the property value of Things.
+            stringNumbers.things = new List<string>();
+            stringNumbers.things.Add("Uno");
+            stringNumbers.things.Add("Dos");
+            stringNumbers.things.Add("Tres");
+            stringNumbers.things.Add("Cuatro");
+            stringNumbers.things.Add("Cinco");
+            stringNumbers.things.Add("Seis");
+            stringNumbers.things.Add("Siete");
+            stringNumbers.things.Add("Ocho");
+            stringNumbers.things.Add("Nueve");
+            stringNumbers.things.Add("Diez");
+
+            //Instantiate an Employee object with type “int” as its generic parameter
+            Employee<int> intNumbers = new Employee<int>();
+            //Assign a list of integers as the property value of Things
+            intNumbers.things = new List<int>();
+            intNumbers.things.Add(1);
+            intNumbers.things.Add(2);
+            intNumbers.things.Add(3);
+            intNumbers.things.Add(4);
+            intNumbers.things.Add(5);
+            intNumbers.things.Add(6);
+            intNumbers.things.Add(7);
+            intNumbers.things.Add(8);
+            intNumbers.things.Add(9);
+            intNumbers.things.Add(10);
+
+            //Print all items to the console
+            foreach (string stN in stringNumbers.things)
+            {
+                Console.WriteLine(stN);
+            }
+
+            foreach (int itN in intNumbers.things)
+            {
+                Console.WriteLine(itN);
+            }
+
+            Console.ReadLine();
         }
     }
 }
