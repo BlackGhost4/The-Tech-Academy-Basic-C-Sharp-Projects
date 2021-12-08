846414e8255ebb79ad266793b5906ba08e43f7c3
BlackGhost4 <60488987+BlackGhost4@users.noreply.github.com>
2021-12-07 16:28:22 -0800

    STRUCT ASSIGNMENT (Step 331)

---
+++ Basic_C#_Programs/StructStep331/StructStep331/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace StructStep331
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //In the next line the data Type Number is created and send the decimal value "234.23"
+            Number number = new Number(Convert.ToDecimal(234.23));
+            //Print the value of the "Amount" on the console
+            Console.WriteLine("The amount is " + number.Amount);
+            Console.ReadLine();
+        }
+
+        //The struct "Number" is created
+        public struct Number
+        {
+            //The constructor receives a decimal value and stores it in the variable Amount
+            public Number(decimal value)
+            {
+                Amount = value;
+            }
+
+            //Property "Amount" of the Struct "Number"
+            public decimal Amount { get; init;  }
+        }
+    }
+}
