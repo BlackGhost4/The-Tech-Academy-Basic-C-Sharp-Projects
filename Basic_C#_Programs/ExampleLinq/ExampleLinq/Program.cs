8c341b91bb9304e3d26256780f0a5473f926fff6
BlackGhost4 <60488987+BlackGhost4@users.noreply.github.com>
2021-12-09 15:30:04 -0800

    Web Application

---
+++ Basic_C#_Programs/ExampleLinq/ExampleLinq/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExampleLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Specify the data source
+            string[] students = new string[] { "Lacey", "Jhon", "Javier", "Mia"};
+
+            //Define the expression
+            IEnumerable<string> studentQuery =
+                from student in students
+                where student.Length >= 4
+                select student;
+
+            //Execute the query
+            foreach (string s in studentQuery)
+            {
+                Console.WriteLine(s);
+            }
+            Console.ReadLine();
+        }
+    }
+}
