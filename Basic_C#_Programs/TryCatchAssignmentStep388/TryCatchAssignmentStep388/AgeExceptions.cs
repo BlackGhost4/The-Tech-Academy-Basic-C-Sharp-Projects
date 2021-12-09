0d19e5c4cc81d578ef5dd161b4ab069f0c9b1d3f
BlackGhost4 <60488987+BlackGhost4@users.noreply.github.com>
2021-12-08 22:02:51 -0800

    TRY/CATCH ASSIGNMENT (Step 388)

---
+++ Basic_C#_Programs/TryCatchAssignmentStep388/TryCatchAssignmentStep388/AgeExceptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchAssignmentStep388
+{
+    public class AgeExceptions : Exception
+    {
+        public AgeExceptions()
+            : base() { }
+        public AgeExceptions(string message)
+            : base(message) { }
+    }
+}
---
+++ Basic_C#_Programs/TryCatchAssignmentStep388/TryCatchAssignmentStep388/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace TryCatchAssignmentStep388
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //block try/catch for catch any exception
+            try
+            {
+                //variable age
+                int age = 0;
+                //ask the user his age
+                Console.WriteLine("Please enter your age");
+                //store the data in variable data
+                string data = Console.ReadLine();
+                //try parse the data to int
+                if (int.TryParse(data, out age))
+                {
+                    //if age is less than zero
+                    if (age < 0)
+                    {
+                        //An AgeExceptions is thrown
+                        throw new AgeExceptions("You must type a positive number");
+                    }
+                    else
+                    {
+                        //Calculate the year of born
+                        Console.WriteLine("You were born in {0}", DateTime.Now.AddYears(age * -1).Year);
+                    }
+                }
+                else
+                {
+                    //if it was not possible to pass the data, an Exception is thrown
+                    throw new Exception("You must type a valid number");
+                }
+            }
+            //Catch the exception of type AgeEsception
+            catch (AgeExceptions e)
+            {
+                Console.WriteLine("Error!!!! -> {0}", e.Message);
+            }
+            //Catch any other exception
+            catch (Exception e)
+            {
+                Console.WriteLine("Error!!!! -> {0}", e.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
