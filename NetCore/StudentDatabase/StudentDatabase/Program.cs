b472866931a8d6a4b1fe5b0ec1fafc980721f832
BlackGhost4 <60488987+BlackGhost4@users.noreply.github.com>
2021-12-16 12:16:38 -0800

    FINAL CHALLENGE SUBMISSION (Step 514)

---
+++ NetCore/StudentDatabase/StudentDatabase/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace StudentDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new SchoolContext())
+            {
+                var student = new Student() { StudentName = "Engels" };
+
+                ctx.Students.Add(student);
+                ctx.SaveChanges();
+
+                Console.WriteLine("Record saved successfully!");
+
+            }
+        }
+        }
+    }
+}
